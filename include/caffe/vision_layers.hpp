0acd66f096901eb575f86677533adf4c8cc23db2
Roberto DiCecco <dicecco.rob@gmail.com>
2015-06-10 14:48:15 -0400

    Added destructor to handle the kernel/program upon completion

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -30,6 +30,10 @@ class BaseConvolutionLayer : public Layer<Dtype> {
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
 
+#ifdef USE_OCL
+  virtual ~BaseConvolutionLayer();
+#endif
+
   virtual inline int MinBottomBlobs() const { return 1; }
   virtual inline int MinTopBlobs() const { return 1; }
   virtual inline bool EqualNumBottomTopBlobs() const { return true; }
