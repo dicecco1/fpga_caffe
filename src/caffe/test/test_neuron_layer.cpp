d62f2f7f8972a724da3c5ce48b0952997d6571e8
dicecco1 <dicecco.rob@gmail.com>
2015-11-02 14:18:01 +0000

    Modified ReLU test

--- src/caffe/test/test_neuron_layer.cpp
+++ src/caffe/test/test_neuron_layer.cpp
@@ -723,7 +723,31 @@ TYPED_TEST(CuDNNNeuronLayerTest, TestTanHGradientCuDNN) {
 #endif
 
 #ifdef USE_OCL
-TYPED_TEST(NeuronLayerTest, TestReLUOCL) {
+template <typename TypeParam>
+class OCLNeuronLayerTest : public MultiDeviceTest<TypeParam> {
+  typedef typename TypeParam::Dtype Dtype;
+ protected:
+  OCLNeuronLayerTest()
+      : blob_bottom_(new Blob<Dtype>(2, 8, 16, 16)),
+        blob_top_(new Blob<Dtype>()) {
+    Caffe::set_random_seed(1701);
+    // fill the values
+    FillerParameter filler_param;
+    GaussianFiller<Dtype> filler(filler_param);
+    filler.Fill(this->blob_bottom_);
+    blob_bottom_vec_.push_back(blob_bottom_);
+    blob_top_vec_.push_back(blob_top_);
+  }
+  virtual ~OCLNeuronLayerTest() { delete blob_bottom_; delete blob_top_; }
+  Blob<Dtype>* const blob_bottom_;
+  Blob<Dtype>* const blob_top_;
+  vector<Blob<Dtype>*> blob_bottom_vec_;
+  vector<Blob<Dtype>*> blob_top_vec_;
+};
+
+TYPED_TEST_CASE(OCLNeuronLayerTest, TestDtypesAndDevices);
+
+TYPED_TEST(OCLNeuronLayerTest, TestReLUOCL) {
   Caffe::set_mode(Caffe::OCL);
   typedef typename TypeParam::Dtype Dtype;
   LayerParameter layer_param;
