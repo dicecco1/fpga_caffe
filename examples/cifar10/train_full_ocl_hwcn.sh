e6c3a836d227d91f7bd73598a283a6b27d4a90e1
dicecco1 <dicecco.rob@gmail.com>
2017-07-17 03:31:52 +0000

    Updating CIFAR10 training files

--- examples/cifar10/train_full_ocl_hwcn.sh
+++ examples/cifar10/train_full_ocl_hwcn.sh
@@ -10,8 +10,3 @@ $TOOLS/caffe train \
 $TOOLS/caffe train \
     --solver=examples/cifar10/cifar10_full_solver_lr1_ocl_hwcn.prototxt \
     --snapshot=examples/cifar10/cifar10_full_iter_8000.solverstate.h5 $@ --ocl=1
-
-# reduce learning rate by factor of 10
-$TOOLS/caffe train \
-    --solver=examples/cifar10/cifar10_full_solver_lr2_ocl_hwcn.prototxt \
-    --snapshot=examples/cifar10/cifar10_full_iter_18000.solverstate.h5 $@ --ocl=1
