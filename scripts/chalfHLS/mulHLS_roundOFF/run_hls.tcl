92ad4026930aa788ad28a411ad7d023a7c8e7c31
apobec3f <zac.anthropomorphism@gmail.com>
2017-07-28 16:37:47 -0400

    Add files via upload

---
+++ scripts/chalfHLS/mulHLS_roundOFF/run_hls.tcl
@@ -0,0 +1,19 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2016 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project mul_prj
+set_top mul
+add_files mul.cpp
+add_files half.hpp
+add_files para.hpp
+add_files -tb adders_test.c
+open_solution "solution1"
+set_part {xcku115-flva1517-2-e}
+create_clock -period 4 -name default
+#source "./adders_prj/solution1/directives.tcl"
+# csim_design -compiler gcc
+csynth_design
+# cosim_design
+export_design -flow impl -rtl verilog -format ip_catalog
